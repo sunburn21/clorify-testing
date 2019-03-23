0291fd75587ee2f624a5b17b049cf353bfa660d7
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2019-03-24 03:39:51 +0530

    rendered index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-  res.send("HIIII!")
+  res.render('./public/index.html')
 })
 app.listen(3000, () => {
   console.log("Server is up!")

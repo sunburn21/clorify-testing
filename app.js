b71226e1e097d11adbd62079ed564bac641c2057
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2019-03-24 04:21:49 +0530

    sadf

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-  res.render('./public/index.html')
+  res.render('./public/index.html');
 })
 app.listen(port, () => {
   console.log("Server is up!")
